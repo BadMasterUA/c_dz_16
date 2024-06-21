d5248db5a1f345e9ca4b7788107f743c62bc82c8
DGHaZe <34streets@gmail.com>
2024-06-21 15:23:37 +0300

    eddit mis_2

--- mis_2.cpp
+++ mis_2.cpp
@@ -10,7 +10,14 @@ public:
     virtual ~Vehicle(){}
     virtual int GetSpeed() = 0;
     virtual int GetPassengers() = 0;
+    //специально сделал функцию для замещения
+    virtual void VehicleColor() = 0;
 };
+
+void Vehicle::VehicleColor()
+{
+    cout << "Vehicle has white color.\n";
+}
 
 //класс Car потомок
 class Car : public Vehicle
@@ -21,12 +28,19 @@ public:
     int GetSpeed() { return itsSpeed; } //описываем функцию для Car
     int GetPassengers() {return itsPassengers; }
     virtual int GetMaxSpeed() = 0;
+    void VehicleColor();
 
 private:
     int itsSpeed;
     int itsPassengers;
     //int itsMaxSpeed;
 };
+
+void Car::VehicleColor()
+{
+    cout << "Car has sports red color!\n";
+    Vehicle::VehicleColor();
+}
 //класс Bus потомок
 class Bus : public Vehicle
 {
@@ -35,16 +49,57 @@ public:
     ~Bus(){}
     int GetSpeed() { return itsSpeed; } //описываем функцию для Car
     int GetPassengers() {return itsPassengers; }
+    void VehicleColor();
 
 private:
     int itsSpeed;
     int itsPassengers;
 };
 
+//класс SportCar
+class SportCar : public Car
+{
+public:
+    SportCar(int speed, int pas, int maxSpeed) : Car(speed, pas), itsMaxSpeed(maxSpeed){}
+    ~SportCar(){}
+    int GetMaxSpeed() { return itsMaxSpeed; }
+
+private:
+    int itsSpeed;
+    int itsPassengers;
+    int itsMaxSpeed;
+};
+
+//класс Wagon
+class Wagon : public Car
+{
+public:
+    Wagon(int speed, int pas, int maxSpeed) : Car(speed, pas), itsMaxSpeed(maxSpeed){}
+    ~Wagon(){}
+    int GetMaxSpeed() { return itsMaxSpeed; }
+
+private:
+    int itsSpeed;
+    int itsPassengers;
+    int itsMaxSpeed;
+};
+
+//класс Coupr 
+class Coupr : public Car
+{
+public:
+    Coupr(int speed, int pas, int maxSpeed) : Car(speed, pas), itsMaxSpeed(maxSpeed){}
+    ~Coupr(){}
+    int GetMaxSpeed() { return itsMaxSpeed; }
+
+private:
+    int itsSpeed;
+    int itsPassengers;
+    int itsMaxSpeed;
+};
+
 int main()
 {
     //test
-    Car *myCar = nullptr;
-    myCar = new Car(200, 2);
     return 0;
 }
