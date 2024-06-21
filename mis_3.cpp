83316693eedbb60198e982f32805658239a3aa33
DGHaZe <34streets@gmail.com>
2024-06-21 16:58:40 +0300

    add mis_3

---
+++ mis_3.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+//создаем класс
+class Hero
+{
+public:
+    Hero(int heath): itsHeath(heath){ Mana++; }
+    virtual ~Hero(){ Mana--; }
+    virtual int GetHeath() { return itsHeath; }
+    virtual void SetHeath(int heath) { itsHeath = heath; }
+    static int GetMana() { return Mana; }
+
+private:
+    int itsHeath;
+    static int Mana;
+};
+
+//инициализируем статическую переменную
+int Hero::Mana = 0;
+
+int main()
+{
+    /* test
+    const int MaxMana = 10; //максимальное значение
+    Hero *ManaPoints[MaxMana];
+    for (int i = 0; i < MaxMana; i++)
+    {
+        ManaPoints[i] = new Hero(i);
+    }
+    for (int i = 0; i < MaxMana; i++)
+    {
+        cout << "Your hero has points of mana " << MaxMana << "/" << ManaPoints[i]->GetMana() << " points.\n";
+
+        delete ManaPoints[i];
+        ManaPoints[i] = nullptr;
+    }*/
+
+    return 0;
+}
